2e1429aafee21db1ba64e7a150d4f33a208d2a10
Silvester Yu <ly1164@nyu.edu>
2022-05-16 22:19:32 +0400

    .

--- detect.py
+++ detect.py
@@ -178,6 +178,7 @@ def run(
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    print("!!!!!!!!!!!!!!!!!!")
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
