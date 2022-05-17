708ed0175bc1d8e8eb8c4729caa877f99be5b1f9
Silvester Yu <ly1164@nyu.edu>
2022-05-17 12:06:07 +0400

    '

--- utils/plots.py
+++ utils/plots.py
@@ -72,17 +72,20 @@ class Annotator:
         non_ascii = not is_ascii(example)  # non-latin labels, i.e. asian, arabic, cyrillic
         self.pil = pil or non_ascii
         # --
-        print("--------------hi here is annotation ----------------")
+        print("--------------(plot.py) hi here is annotation ----------------")
         # --
         if self.pil:  # use PIL
             # --
-            print("-------- hi using pillow ------------")
+            print("-------- (plot.py) hi using pillow ------------")
             # --
             self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
             self.draw = ImageDraw.Draw(self.im)
             self.font = check_pil_font(font='Arial.Unicode.ttf' if non_ascii else font,
                                        size=font_size or max(round(sum(self.im.size) / 2 * 0.035), 12))
         else:  # use cv2
+            # --
+            print("-------- (plot.py) hi using cv2 ------------")
+            # --
             self.im = im
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
 
