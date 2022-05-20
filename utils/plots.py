e23c67a76cba9a48f001b55b219c7be5c459ab09
Silvester Yu <ly1164@nyu.edu>
2022-05-20 12:55:11 +0400

    。

--- utils/plots.py
+++ utils/plots.py
@@ -83,6 +83,10 @@ class Annotator:
 
     def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
         # Add one xyxy box to image with label
+        print("%"*100)
+        print("This is from plots.py Annotator class box_label() function, argument box is as follows")
+        print(box)
+        print("%"*100)
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
             if label:
