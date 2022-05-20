e9b553a6df32dad12837af744e7768cbe08c2bae
Silvester Yu <ly1164@nyu.edu>
2022-05-21 00:12:42 +0400

    +

--- utils/plots.py
+++ utils/plots.py
@@ -158,7 +158,8 @@ class Annotator:
                 cv2.putText(self.im, 'OpenCV', org, font, 
                                 fontScale, color, thickness, cv2.LINE_AA)
                 cv2.putText(self.im,
-                            label, (center_x, center_y - axis_y/2 if outside else center_x, center_y - axis_y/2),
+                            label, (center_x, center_y - axis_y/2 if outside else center_y + axis_y/2),
+                            0,
                             self.lw / 3,
                             color,
                             thickness=tf
