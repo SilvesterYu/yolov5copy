3c3cbae7173bd7550762ea323a3de2632987fe6f
Silvester Yu <ly1164@nyu.edu>
2022-05-20 13:29:59 +0400

    。

--- utils/plots.py
+++ utils/plots.py
@@ -98,6 +98,7 @@ class Annotator:
         else:  # cv2
             p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
             cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
+            if label:
                 tf = max(self.lw - 1, 1)  # font thickness
                 w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
                 outside = p1[1] - h >= 3
