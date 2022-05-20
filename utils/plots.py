2f8ec57abeb984c35bf8159a0ccd5b8fd8254662
Silvester Yu <ly1164@nyu.edu>
2022-05-20 22:40:03 +0400

    +

--- utils/plots.py
+++ utils/plots.py
@@ -128,7 +128,7 @@ class Annotator:
 
                 # -- ellipse -- #
                 cv2.ellipse(self.im, center_coordinates, axesLength, angle,
-                                    startAngle, endAngle, color, thickness)
+                                    startAngle, endAngle, color, thickness, cv2.LINE_AA)
                 # --
 
                 cv2.putText(self.im,
