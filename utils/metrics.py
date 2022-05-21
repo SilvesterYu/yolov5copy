2f7b25dc795a4725fa668e7be3ac31dbc5db5cc8
Silvester Yu <ly1164@nyu.edu>
2022-05-21 14:03:18 +0400

    +

--- utils/metrics.py
+++ utils/metrics.py
@@ -309,7 +309,8 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-7
             (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
 
     print("+++++++++ checking tensor b1_x2 +++++++++")
-
+    ellipse3 = create_ellipse((b1_x, b1_y),(b1_halfw, b1_halfh),0)
+    print(ellipse3)
 
     # Union Area
     union = w1 * h1 + w2 * h2 - inter + eps
